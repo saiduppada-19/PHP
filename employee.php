d324386c49b8d3041c195060d1739baf438dcaed
Gnanendra sai duppada <saiconvos@gmail.com>
2026-07-16 23:36:00 +0530

    Add employee detail entry form in employee.php

---
+++ employee.php
@@ -0,0 +1,22 @@
+<head>
+<title>EMPLOYEE DETAIL ENTRY FORM</title>
+</head>
+<body>
+<?php
+$ID=201;
+$Name='sandhya';
+$Gender='Female';
+$Salary='30,000';
+$department='Computer Science';
+$post='Lecturer';
+$Address='Kakinada';
+echo "EMPLOYEE DETAILS<br>";
+echo 'Employe ID: '.$id.'<br>';
+echo 'Employe Name : '.$Name.'<br>'; 
+echo 'Employe Gender:'.$Gender.'<br>'';
+echo 'Employe Salary : '.$Salary.'<br>';
+echo 'Employee Working Department :'.$department.'<br>';
+echo 'Employee Working post:'.$post.'<br>';
+echo 'Employe address:'.$Address;
+</body>
+?>
